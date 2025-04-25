5cdfd607363f8f1507e477cb0ad7d6d8bf3ed20f
Badal Rathore <badalsinghrathore89@gmail.com>
2025-04-25 18:25:58 +0530

    changes commited

---
+++ frontend/index.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const app=express();
+app.use("/static",express.static("public/images"));
+app.use(express.static("views"));
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000");
+});
